47606916a929f27814c2a7699fc0484e5cf91fb1
EvertonKessler <evertonkessler@yahoo.com.br>
2019-03-28 13:01:24 -0300

    Movimentos possiveis para uma peça

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -42,7 +42,10 @@ public class ChessMatch {
 	
 	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)) {
-			throw new ChessException("Não existe peça na posição: " + position);
+			throw new ChessException("Nao existe peca na posicao: " + position);
+		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Nao ha movimentos possiveis para a peca");
 		}
 	}
 	
